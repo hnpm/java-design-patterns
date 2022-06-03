68c4f6b5f4c4591d31543d1037e63a177b2cd4b4
Hien Pham <h13nph4m@gmail.com>
2022-06-03 11:15:39 +0700

    abstract factory

---
+++ src/abstract_factory/BikeFactory.java
@@ -0,0 +1,12 @@
+package abstract_factory;
+
+public class BikeFactory extends VehicleFactory {
+
+    @Override
+    public Vehicle create(VehicleSize size) {
+        return switch (size) {
+            case SMALL -> new MountainBike();
+            case LARGE -> new CityBike();
+        };
+    }
+}
